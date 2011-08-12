c07c8a1e8c2c4b8a24e1c090513ec07828e8a3ef
Flemming Frandsen <dren.dk@gmail.com>
2011-08-12 19:27:49 +0200

    Works on the test bench now

--- src/blaupunkt-2011.h
+++ src/blaupunkt-2011.h
@@ -18,23 +18,23 @@ const Remote BLAUPUNKT_2011 = {
 };
 
 //Key codes:
-const unsigned int BLAUPUNKT_2011_DOWN = 0xE817;
-const unsigned int BLAUPUNKT_2011_UP = 0x6897;
-const unsigned int BLAUPUNKT_2011_POWER = 0xF00F;
-const unsigned int BLAUPUNKT_2011_MUTE = 0x28D7;
-const unsigned int BLAUPUNKT_2011_GREEN = 0x08F7;
-const unsigned int BLAUPUNKT_2011_MENU = 0xA857;
-const unsigned int BLAUPUNKT_2011_ESC = 0x708F;
-const unsigned int BLAUPUNKT_2011_1 = 0x807F;
-const unsigned int BLAUPUNKT_2011_VOLUMEDOWN = 0xD02F;
-const unsigned int BLAUPUNKT_2011_5 = 0xA05F;
-const unsigned int BLAUPUNKT_2011_4 = 0x20DF;
-const unsigned int BLAUPUNKT_2011_RIGHT = 0x10EF;
-const unsigned int BLAUPUNKT_2011_SEARCH = 0xC837;
-const unsigned int BLAUPUNKT_2011_VOLUMEUP = 0x30CF;
-const unsigned int BLAUPUNKT_2011_2 = 0x40BF;
-const unsigned int BLAUPUNKT_2011_LEFT = 0x906F;
-const unsigned int BLAUPUNKT_2011_RED = 0x48B7;
-const unsigned int BLAUPUNKT_2011_3 = 0xC03F;
+#define BLAUPUNKT_2011_DOWN 0xE817
+#define BLAUPUNKT_2011_UP 0x6897
+#define BLAUPUNKT_2011_POWER 0xF00F
+#define BLAUPUNKT_2011_MUTE 0x28D7
+#define BLAUPUNKT_2011_GREEN 0x08F7
+#define BLAUPUNKT_2011_MENU 0xA857
+#define BLAUPUNKT_2011_ESC 0x708F
+#define BLAUPUNKT_2011_1 0x807F
+#define BLAUPUNKT_2011_VOLUMEDOWN 0xD02F
+#define BLAUPUNKT_2011_5 0xA05F
+#define BLAUPUNKT_2011_4 0x20DF
+#define BLAUPUNKT_2011_RIGHT 0x10EF
+#define BLAUPUNKT_2011_SEARCH 0xC837
+#define BLAUPUNKT_2011_VOLUMEUP 0x30CF
+#define BLAUPUNKT_2011_2 0x40BF
+#define BLAUPUNKT_2011_LEFT 0x906F
+#define BLAUPUNKT_2011_RED 0x48B7
+#define BLAUPUNKT_2011_3 0xC03F
 
 #endif
--- src/bpremote.c
+++ src/bpremote.c
@@ -23,53 +23,165 @@
 
 #define MIN_DELAY 18
 
+// This maps from adc values to the remote control codes:
+const struct {
+	unsigned int adc;
+	unsigned int code;
+}adc2code[6] = {
+		{1023, 0},
+		{710, BLAUPUNKT_2011_MUTE},
+		{532, BLAUPUNKT_2011_SEARCH},
+		{356, BLAUPUNKT_2011_UP},
+		{186, BLAUPUNKT_2011_VOLUMEUP},
+		{0, BLAUPUNKT_2011_VOLUMEDOWN}
+};
+
+// We don't really care about unhandled interrupts.
+EMPTY_INTERRUPT(__vector_default)
+
 void delay(unsigned int us) {
 	_delay_loop_2(us << 1);
+}
+
+void initADC(void){
+ ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));    //16Mhz/128 = 125Khz the ADC reference clock
+ ADMUX |= (1<<REFS0);                //Voltage reference from Avcc (5v)
+ ADCSRA |= (1<<ADEN);                //Turn on ADC
+ ADCSRA |= (1<<ADSC);                //Do an initial conversion because this one is the slowest and to ensure that everything is up and running
+}
+
+uint16_t getADC_(uint8_t channel){
+ ADMUX &= 0xF0;                    //Clear the older channel that was read
+ ADMUX |= channel;                //Defines the new ADC channel to be read
+ ADCSRA |= (1<<ADSC);                //Starts a new conversion
+ while(ADCSRA & (1<<ADSC));            //Wait until the conversion is done
+ return ADCW;                    //Returns the ADC value of the chosen channel
+}
+
+
+
+unsigned int getADC(unsigned char input) {
+    ADMUX = (input & 15) | _BV(REFS0); // AVcc reference + external cap.
+    ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0) | _BV(ADIE) | _BV(ADEN);
+
+    sei();
+
+    if (1) {
+    	ADCSRA |= 1<<ADSC;
+    	while(ADCSRA & 1<<ADSC) {}
+    } else {
+    	SREG |= _BV(SREG_I);
+    	set_sleep_mode(SLEEP_MODE_ADC);
+    	sleep_enable();
+    	sleep_mode();
+    }
+
+    return ADCW;
+//    unsigned int result = ADCL | ADCH << 8;
+
+//    return result;
+}
+
+unsigned int getCurrentCode() {
+	unsigned int in = getADC(0);
+
+	unsigned int best = 0;
+	unsigned int bestDist = 0xffff;
+	for (int i=0;i<6;i++) {
+		int dist = in-adc2code[i].adc;
+		if (dist < 0) {
+			dist = -dist;
+		}
+
+		if (dist < bestDist) {
+			best = adc2code[i].code;
+			bestDist = dist;
+		}
+	}
+
+	if (bestDist < 50) {
+		return best;
+	} else {
+		return 0;
+	}
 }
 
 int main(void) {
   wdt_enable(WDTO_4S);
 
+  initADC();
   uart_init();
   DDRC = _BV(PC1);
   PORTC |= _BV(PC1);
 
   fprintf(stdout, "Power up!\n");
   delay(64000);
-  fprintf(stdout, "Done sleeping...\n");
 
 
   RemoteState rs;
-
+/*
   char runCode = 1;
   remoteStart(&rs, &BLAUPUNKT_2011, BLAUPUNKT_2011_POWER);
-/*
-  fprintf(stdout, "Done starting code...\n");
-  while (!remoteDone(&rs)) {
-    int time = remoteSignal(&rs);
-    if (time < 0) {
-    	fprintf(stdout, "Space: %4d  state:%d  codestate:%d\n", -time, rs.state, rs.codeState);
-    } else {
-    	fprintf(stdout, "Mark:  %4d  state:%d  codestate:%d\n", time, rs.state, rs.codeState);
-    }
+  if (runCode) {
+	  int time = remoteSignal(&rs);
+
+	  if (time > 0) {
+		  PORTC &=~ _BV(PC1);
+		  delay(time);
+	  } else {
+		  PORTC |= _BV(PC1);
+		  delay(-time);
+	  }
+
+	  if (remoteDone(&rs)) {
+		  runCode = 0;
+	  }
   }
-  fprintf(stdout, "Done printing codes...\n");
 */
+
+
+  unsigned int current = 0;
+  unsigned int steady = 0;
+  unsigned int activeCode = 0;
   while (1) {
+	  unsigned int nc = getCurrentCode();
 
-	  if (runCode) {
-		  int time = remoteSignal(&rs);
+	  if (current == nc) {
+		if (steady < 20) {
+			steady++;
+		}
+	  } else {
+		current = nc;
+	  }
 
-		  if (time > 0) {
-			  PORTC &=~ _BV(PC1);
-			  delay(time);
-		  } else {
-			  PORTC |= _BV(PC1);
-			  delay(-time);
+	  if (steady >= 3) {
+		  if (activeCode != current) {
+
+			  if (current) {
+				  remoteStart(&rs, &BLAUPUNKT_2011, current);
+				  fprintf(stdout, "New code: %4x\n", current);
+			  }
+
+			  activeCode = current;
 		  }
+	  }
 
-		  if (remoteDone(&rs)) {
-			  runCode = 0;
+	  if (activeCode) {
+
+		  while (1) {
+			  int time = remoteSignal(&rs);
+
+			  if (time > 0) {
+				  PORTC &=~ _BV(PC1);
+				  delay(time);
+			  } else {
+				  PORTC |= _BV(PC1);
+				  delay(-time);
+			  }
+
+			  if (remoteDone(&rs)) {
+				  break;
+			  }
 		  }
 	  }
 
